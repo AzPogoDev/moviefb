d53fc189ab1945eeb760191cd59d3e7aecd73273
henri <henricousin07@gmail.com>
2021-09-29 09:05:57 +0200

    Project work with favorites

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\FavoriteMovieRepository;
 use App\Repository\MovieEntityRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -12,6 +13,22 @@ use Symfony\Component\HttpFoundation\ParameterBag;
 class HomeController extends AbstractController
 {
 
+
+    /**
+     * @var FavoriteMovieRepository
+     */
+    private FavoriteMovieRepository $favoriteMovieRepository;
+    /**
+     * @var MovieEntityRepository
+     */
+    private MovieEntityRepository $movieEntityRepository;
+
+    public function __construct(FavoriteMovieRepository $favoriteMovieRepository, MovieEntityRepository $movieEntityRepository)
+    {
+        $this->favoriteMovieRepository = $favoriteMovieRepository;
+        $this->movieEntityRepository = $movieEntityRepository;
+    }
+
     /**
      * @Route("/", name="home")
      */
@@ -20,6 +37,21 @@ class HomeController extends AbstractController
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
             'movies' => $movieEntityRepository->getPopularMovies()
+        ]);
+    }
+
+    /**
+     * @Route("movie/add/{title}/", name="add_favorite")
+     * @param $title
+     * @param Request $request
+     * @return Response
+     */
+    public function addToFavorite($title, Request $request): Response
+    {
+        $this->favoriteMovieRepository->addFavorite($title, $request->get('id'));
+
+        return $this->render('home/index.html.twig', [
+            'movies' => $this->movieEntityRepository->getPopularMovies()
         ]);
     }
 }
--- src/Controller/MovieController.php
+++ src/Controller/MovieController.php
@@ -2,10 +2,12 @@
 
 namespace App\Controller;
 
+use App\Repository\FavoriteMovieRepository;
 use App\Repository\MovieEntityRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Security;
 
 class MovieController extends AbstractController
 {
@@ -19,6 +21,27 @@ class MovieController extends AbstractController
     {
         return $this->render('movie/index.html.twig', [
             'movie' => $repo->findMovie($id),
+        ]);
+    }
+
+
+    /**
+     * @Route("/favorites", name="favorites")
+     * @param FavoriteMovieRepository $favoriteMovieRepository
+     * @param Security $security
+     * @return Response
+     */
+    public function favorites(FavoriteMovieRepository $favoriteMovieRepository, Security $security): Response
+    {
+        if ($security->getUser()) {
+            $userconencted = $security->getUser()->getUserIdentifier();
+        }
+        $moviesfav = $favoriteMovieRepository->getFavoritesMovies($security->getUser()->getUserIdentifier());
+
+        return $this->render('favorite/index.html.twig', [
+            'controller_name' => 'MovieController',
+            'favmovies' => $moviesfav,
+            'couser' => $userconencted ?? ''
         ]);
     }
 
