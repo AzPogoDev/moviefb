912e75ac7d6cd44dd501748e10b40b51190598d8
henri <henricousin07@gmail.com>
2021-10-18 10:14:39 +0200

    403 response

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\Exception\AccessDeniedException;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Security;
@@ -23,7 +24,8 @@ class SecurityController extends AbstractController
             return $this->render('user/myaccount.html.twig',
                 ['user' => $userconencted]);
         }
-        return new Response("This action needs a valid token!", Codes::HTTP_FORBIDDEN);
+        $content = '403 Acces denied';
+        return new Response($content, 403);
 
     }
 
